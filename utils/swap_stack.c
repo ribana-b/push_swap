43e10a30e65c7d240f70cc475ec8543d19cc2065
SrRecursive <rojohnibana@gmail.com>
2024-01-15 12:06:44 +0100

    Fix condition where next pointer of the stack is NULL

--- utils/swap_stack.c
+++ utils/swap_stack.c
@@ -6,7 +6,7 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 23:47:07 by ribana-b          #+#    #+#             */
-/*   Updated: 2023/11/15 02:28:37 by ribana-b         ###   ########.fr       */
+/*   Updated: 2024/01/07 23:53:01 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	swap_stack_a(t_stack *stacka)
 {
 	int	temp;
 
-	if (!stacka)
+	if (!stacka || !stacka->next)
 		return ;
 	temp = stacka->value;
 	stacka->value = stacka->next->value;
@@ -28,7 +28,7 @@ void	swap_stack_b(t_stack *stackb)
 {
 	int	temp;
 
-	if (!stackb)
+	if (!stackb || !stackb->next)
 		return ;
 	temp = stackb->value;
 	stackb->value = stackb->next->value;

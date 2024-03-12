9ff77e3a738a69fba56e97901046389295f1a397
SrVariable <rojohnibana@gmail.com>
2024-03-12 20:20:04 +0100

    Update initialise_info function
    This should have gone in commmit 7ad0f9c1cb0a78ea951be441b0d23a4714d7395f

--- src/utils/info.c
+++ src/utils/info.c
@@ -6,7 +6,7 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 08:06:07 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2024/03/11 13:34:48 by ribana-b         ###   ########.com      */
+/*   Updated: 2024/03/11 14:36:30 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	initialise_info(t_info *info, t_stack **stack_a)
 	info->stack[B] = NULL;
 	info->size_stack[A] = stack_len(*stack_a);
 	info->size_stack[B] = 0;
-	info->max_stack[A] = MIN_INT;
-	info->max_stack[B] = MIN_INT;
-	info->min_stack[A] = MAX_INT;
-	info->min_stack[B] = MAX_INT;
+	info->max_stack[A][VALUE] = MIN_INT;
+	info->max_stack[B][VALUE] = MIN_INT;
+	info->min_stack[A][VALUE] = MAX_INT;
+	info->min_stack[B][VALUE] = MAX_INT;
 	info->movements = NULL;
 }
